8bc22a4335f4b6b4ab9e6e2185f276aaa6533f39
Kanwar007 <vijayblp.kanwar@gmail.com>
2021-12-19 20:07:27 +0800

    --- Auto-Git Commit ---

--- angular-application/src/app/directive/added.directive.ts
+++ angular-application/src/app/directive/added.directive.ts
@@ -1,10 +1,27 @@
-import { Directive } from '@angular/core';
+import { Directive,ElementRef,HostListener, Input } from '@angular/core';
+import { element } from 'protractor';
 
 @Directive({
   selector: '[appAdded]'
 })
 export class AddedDirective {
 
-  constructor() { }
+  constructor(private e1 : ElementRef) { }
+
+   // @Input("format") format;
+    @Input("appAdded") format;
+    @HostListener("focus") onfocus() {
+    console.log("on focus");
+  }
+
+ @HostListener("blur") onBlur() {
+     let value :String = this.e1.nativeElement.value;
+     if(this.format=="lowercase")
+     this.e1.nativeElement.value= value.toLowerCase();
+     else
+     this.e1.nativeElement.value= value.toUpperCase();
+    console.log(" on Blur");
+  }
+  
 
 }
